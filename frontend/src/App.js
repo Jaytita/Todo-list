965bcfda6d06ee0d341d7a11b9095e66a96df5c8
Jaytitaya <6130142121@student.chula.ac.th>
2023-01-05 22:05:22 +0700

    Add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +26,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
