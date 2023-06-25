0a6ba5d5b74f067ca0546d1cfb0b6490ec1c7c00
Ken <yathip@nsquared.asia>
2023-06-25 23:14:09 +0700

    MLD-007 Finish login UI

--- frontend/src/Pages/FEMTask/index.jsx
+++ frontend/src/Pages/FEMTask/index.jsx
@@ -8,6 +8,7 @@ import {
 } from '@mui/icons-material';
 import Sidebar from '../../Components/Sidebar';
 import CopyBar from '../../Components/CopyBar';
+import { Form, Input, Button, Divider } from 'antd'
 
 export default function Task() {
   // Dark mode
@@ -115,6 +116,85 @@ export default function Task() {
 
   //   setTasks(newTasks);
   // };
+
+  const loginPage = () => {
+    return (
+      <div>
+      <Form
+        className="card"
+        name="login"
+        layout="vertical"
+      >
+        <div className="img-container">
+          <h1>MaiLok Dev</h1>
+        </div>
+
+        <div className="input-login-form">
+          <div className="form-inline">
+            <Form.Item
+              key={"email"}
+              rules={[
+                {
+                  required: true,
+                  message: "Email is required!",
+                },
+                {
+                  type: "email",
+                  message: "This is not a valid email!",
+                },
+              ]}
+              name={"email"}
+              label={null}
+            >
+              <Input size="large" placeholder="Email" />
+            </Form.Item>
+          </div>
+
+          <div className="form-inline">
+            <Form.Item
+              key={"password"}
+              rules={[{ required: true, message: "Password is required!" }]}
+              name={"password"}
+              label={null}
+            >
+              <Input
+                size="large"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+          </div>
+        </div>
+
+        <div className="form-inline end-form">
+          <Button
+            size="large"
+            htmlType="submit"
+            type="primary"
+          >
+            <span>Login With Email</span>
+          </Button>
+        </div>
+
+        <div className="form-inline end-line">
+            <GoogleOAuthProvider
+            >
+              <LoginButton />
+            </GoogleOAuthProvider>
+          </div>
+
+        <Divider />
+        <div className="form-inline" style={{ marginTop: "0px" }}>
+          <Button
+            type="link"
+          >
+            Forgot password ?
+          </Button>
+        </div>
+      </Form>
+    </div>
+    )
+  }
 
   return (
     <div className="fem-task-app">
