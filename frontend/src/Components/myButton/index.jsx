6b675f4a5e50079dee5350c000584e44763951b1
Ken <ken.yathip@gmail.com>
2023-07-02 13:44:56 +0700

    cardModal cancel button

--- frontend/src/Components/myButton/index.jsx
+++ frontend/src/Components/myButton/index.jsx
@@ -1,8 +1,19 @@
 import './style.scss';
 
+import { useDispatch } from 'react-redux';
+import { setVisible } from '../../redux/reducers/cardModalReducer';
+
 const MyButton = ({ primary=false, text="" }) => {
+  const dispatch = useDispatch();
+
+  const handleCloseModal = () => {
+    dispatch(setVisible(false));
+  }
   return (
-    <button className={`my-button ${primary ? "primary" : ""}`}>
+    <button
+      className={`my-button ${primary ? "primary" : ""}`}
+      onClick={text === "Cancel" && handleCloseModal}
+    >
       {text}
     </button>
   )
