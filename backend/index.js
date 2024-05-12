3cb2a0be1371eca0398e1daf4bec5ae3e1888c51
Jaytita <titaya_poonpipatnun@yahoo.com>
2024-05-12 15:06:07 +0700

    frontend(Build): Build react app

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 4040;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI;
 
 app.use(express.json());
 
 //Connect MongoDB
-async function connect(){
-    try{
-        await mongoose.connect(uri);
-        console.log('Connect to MongoDB')
-    } catch (error) {
-        console.error(error);
-    }
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connect to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
 }
 connect();
 
 //Database Router
-const usersRouter = require('./routes/users');
-app.use('/users', usersRouter);
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
-app.use('/',(req,res)=>{
-    res.send('Hello Mailok');
-})
+app.use("/", (req, res) => {
+  res.send("Hello Mailok");
+});
 
-app.listen(port,()=>{
-    console.log(`Server start on port ${port}.`);
-})
+app.listen(port, () => {
+  console.log(`Server start on port ${port}.`);
+});
 
 // Export the Express API
 module.exports = app;
